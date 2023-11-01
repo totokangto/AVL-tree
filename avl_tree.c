067b380993544c2c8cafc67f208f6533c5fdac7a
KangHyunLee <kangtoto1@naver.com>
2023-11-01 21:56:07 +0900

    add delete_node.c

--- avl_tree.c
+++ avl_tree.c
@@ -2,45 +2,29 @@
 #include<stdlib.h>
 #include"make_tree_node.h"
 #include"insert_node.h"
-#include"height.h"
+#include"delete_node.h"
 
 int main(void) {
 	AVL_node root = NULL;
 	
 	root = insert_node(root, 3);
 	root = insert_node(root, 2);
-	root = insert_node(root, 1);
+	root = insert_node(root, 1); // R Rotation
 	root = insert_node(root, 4);
-	root = insert_node(root, 5);
-	printf("%d\n", root->height);
-	//printf("%d\n", root->height);
-	/*
-	AVL_node a = (AVL_node)malloc(sizeof(struct node));
-	AVL_node b = (AVL_node)malloc(sizeof(struct node));
-	AVL_node c = (AVL_node)malloc(sizeof(struct node));
-	a->key = 100;
-	a->height = 0;
-	a->left = NULL;
-	a->right = NULL;
-	b->key = 99;
-	b->height = 0;
-	b->left = NULL;
-	b->right = NULL;
-	//c->key = 98;
-	//c->height = 0;
-
-	a->left = b;
-	update_height(a);
-	//b->left = c;
-	//update_height(b);
-	//update_height(a);
-
-	int ah = get_height(a);
-	int bh = get_height(b->left->left);
-	printf("%d\n%d\n", a->left,b);
-	printf("%d\n%d\n", b->left, bh);
-	*/
-	
+	root = insert_node(root, 5); // L Rotation
+	root = insert_node(root, 6);
+	root = insert_node(root, 7); // L Rotation
+	root = insert_node(root, 16);
+	root = insert_node(root, 15); // RL Rotation
+	root = insert_node(root, 14); // LR Rotation
+	printf("root key : %d\n", root->key); // 4
+	printf("root height : %d\n", root->height); // 3
+	root = delete_node(root, 1); // delete node that has no child
+	root = delete_node(root, 2); // delete node that has one child, Single Rotation(Left)
+	root = delete_node(root, 15); // delete node that has two child
+	root = delete_node(root, 16); // Double Rotation(Left Right)
+	printf("root key : %d\n", root->key); // 6
+	printf("root height : %d\n", root->height); // 2
 
 	return 0;
 }
---
+++ delete_node.c
@@ -0,0 +1,68 @@
+#pragma once
+#include<stdio.h>
+#include<stdlib.h>
+#include"insert_node.h"
+#include"height.h"
+#include"rotation.h"
+AVL_node delete_node(AVL_node root, int key) {
+	
+	// 1. node 찾아서 삭제
+	// 없애려는 노드의 key가 root의 key보다 작으면 left subtree로 이동
+	if (key < root->key) {
+		root->left = delete_node(root->left, key);
+	}
+	// 없애려는 노드의 key가 root의 key보다 크면 right subtree로 이동
+	else if (key > root->key) {
+		root->right = delete_node(root->right, key);
+	}
+	// 없애려는 노드의 key = root의 key => 삭제할 node
+	else {
+		if (root->left == NULL || root->right == NULL) {
+			
+			AVL_node temp = root->right ? root->right : root->left;
+			// node has no child
+			if (temp == NULL) {
+				temp = root;
+				root = NULL;
+			}
+			// node has 1 child
+			else
+				// root를 root의 자식 노드로 교체
+				*root = *temp;			
+			free(temp);
+		}
+		else {
+			// root의 left subtree 중 가장 큰 key값을 갖는 node 찾기
+			AVL_node max = root->left;
+			while (max->right != NULL)
+				max = max->right;
+			// root의 key를 left subtree에서 가장 큰 key로 교체
+			root->key = max->key;
+			// root의 left subtree에서 그 노드 삭제
+			root->left = delete_node(root->left, max->key);
+		}
+	}
+
+	// 2. imbalance 잡기
+	if (root == NULL)
+		return root;
+	update_height(root);
+	// balanced factor
+	int bf = get_height(root->left) - get_height(root->right);
+	// left subtree's height > right subtree's height
+	if (bf > 1) {
+		if (get_height(root->left->left) > get_height(root->left->right)) {
+			root = R_Rotation(root);
+		}
+		else root = LR_Rotation(root);
+	}
+	// right subtree's height > left subtree's height
+	else if (bf < -1) {
+		if (get_height(root->right->left) < get_height(root->right->right) || get_height(root->left) == -1) {
+			root = L_Rotation(root);
+		}
+		else root = RL_Rotation(root);
+	}
+
+	return root;
+}
